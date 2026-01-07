eeaa070007ca4642ac4120375400d5d49fc33919
amonhaupt <amon.haupt@aol.com>
2026-01-07 17:44:08 +0100

    revert last commit

--- js/pages/auth.js
+++ js/pages/auth.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:1350';
+const BACKEND_URL = "https://pingme-backend-nu.vercel.app";
 
 async function createUser(user) {
     const res = await fetch(`${BACKEND_URL}/auth/register`, {
