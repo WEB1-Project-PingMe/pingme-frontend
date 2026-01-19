86ddb87cd0d675c6b258f913350e6986aa759530
nishan-singh <singh007nishan@gmail.com>
2026-01-19 10:02:11 +0100

    remove react from mpa repo

--- pingme-spa/src/router.ts
+++
@@ -1,9 +0,0 @@
-import {
-  type RouteConfig,
-  route,
-} from "@react-router/dev/routes";
-
-export default [
-  route("/", "./App.tsx"),
-  route("chat/:uuid", "./component/chat-window.tsx"),
-] satisfies RouteConfig;
